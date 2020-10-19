f9fef8b56b2068c42b5faac61178eedcbfd6eac8
bongofury <jtroutmanman@gmail.com>
2020-10-18 19:04:10 -0500

    Scraper class method .scrape_info_page added

--- lib/Album.rb
+++ lib/Album.rb
@@ -1,2 +1,3 @@
 class Album
+    attr_accessor :name, :artist, :label, :lp, :flac, :mp3, :description
 end
--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -15,10 +15,40 @@ class Scraper
         end
         result
     end
+
+    def self.scrape_info_page(info_url)
+        doc = Nokogiri::HTML(open(info_url))
+        info = {}
+        description = doc.css("div.item-meta p").text
+        doc.css("div.item").each do |format|
+            type = format.css("h5").text
+            type = type.split(" ")
+            date = format.css("p").text
+            date = date.slice(0..9)
+            info[type[0].to_sym] = {
+                :price => type[1],
+                :release_date => date
+            }
+        end
+        info[:description] = description
+        info
+    end
 end
 
+#Release Page-
 #bands = doc.css("div.product-page div.uk-grid div.album")
 #artist = bands.css("h4 a").text
 #album = bands.css("h5").text
 #label = bands.css("h6 a").text
 #info_page = bands.css("a")[0]["href"]
+
+#Album info page-
+#album = doc.css("div.item").each do |format|
+  #type = format.css("h5").text
+  #type = type.split(" ")
+  #date = format.css("p").text
+  #date = date.slice(0..9)
+  #formats[type[0].to_sym] = {
+      #:price => type[1],
+      #:release_date => date
+    }
