ffdbf009d6cc1691c383fc6f4a893d9aa91397a9
bongofury <jtroutmanman@gmail.com>
2020-10-21 12:37:40 -0500

    added .create_albums class method and #add_album_info instance method

--- lib/Album.rb
+++ lib/Album.rb
@@ -7,4 +7,12 @@ class Album
         album_hash.each {|key, value| self.send("#{key}=", value)}
         @@all << self
     end
+
+    def self.create_albums(album_array)
+        album_array.each {|album| Album.new(album)}
+    end
+
+    def add_album_info(info_hash)
+        info_hash.each {|key, value| self.send("#{key}=", value)}
+    end
 end
