ff6918b8da8d5467763bf2a37e5ef83aeef9a1e4
bongofury <jtroutmanman@gmail.com>
2020-10-21 18:49:50 -0500

    added #get_albums and #add_info to cli.rb

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,2 +1,21 @@
+require_relative "../lib/scraper.rb"
+require_relative "../lib/album.rb"
+require 'nokogiri'
+
 class CommandLineInterface
+    ALBUM_URL = "https://www.midheaven.com"
+
+    def get_albums
+        albums = Scraper.scrape_release_page(ALBUM_URL + "/upcoming-releases")
+        Album.create_albums(albums)
+    end
+
+    def add_info
+        Album.all.each do |album|
+            info = Scraper.scrape_info_page(ALBUM_URL + album.info_url)
+            album.add_album_info(info)
+        end
+    end
+
+    
 end
