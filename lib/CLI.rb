4d9a15c10bbbd98cddc1b987670bee98a812ac64
bongofury <jtroutmanman@gmail.com>
2020-10-21 23:08:28 -0500

    added #label_list to cli.rb

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -34,4 +34,22 @@ class CommandLineInterface
         puts "#{album.description}"
        end
     end
+
+    def label_list
+        list = Album.all.sort_by {|album| album.label}
+        list.each_with_index do |album, i|
+         puts "#{i + 1}. Label: #{album.label}   Artist: #{album.artist}   Album: #{album.name}"
+        end
+        input = ""
+        until input >= 0 && input < list.count
+         puts "Enter number of album for more information"
+         input = gets.strip.to_i - 1
+         album = list[input]
+         puts "LP: #{album.lp[price]}, #{album.lp[release_date]}"
+         puts "CD: #{album.cd[price]}, #{album.cd[release_date]}"
+         puts "MP3: #{album.mp3[price]}, #{album.mp3[release_date]}"
+         puts " "
+         puts "#{album.description}"
+        end
+     end
 end
